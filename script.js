fa82f9d1ba165143884ed3f43a40d4be72b461f5
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-11-14 13:51:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+
+let year=prompt();
+function daysOfAYear(year){
+	if (year % 400 == 0) {
+     return 366;
+   }
+   else if (year % 100 == 0) {
+    return 365;
+   }
+   else if (year % 4 == 0) {
+	   return 366;
+   }
+   else {
+      return 365
+   }
+}
+let z=daysOfAYear(year);
+alert(z);
